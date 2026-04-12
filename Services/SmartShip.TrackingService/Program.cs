c7de5f337a86260d334c99ee3eb81eba1af8bc09
Saurabh Rana <ranasaurabh191@gmail.com>
2026-04-12 15:59:22 +0530

    Tracking CICD

--- Services/SmartShip.TrackingService/Program.cs
+++ Services/SmartShip.TrackingService/Program.cs
@@ -23,7 +23,7 @@ Log.Logger = new LoggerConfiguration()
 
 try
 {
-    Log.Information(" --> Starting TrackingService...");
+    Log.Information(" --> 12Starting TrackingService...");
 
     var builder = WebApplication.CreateBuilder(args);
 
